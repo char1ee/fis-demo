7a2df2c4e82b0cc9971b8ab82a4921650277f7c2
Jerry <jerry@ubuntu.ubuntu-domain>
2013-08-16 01:35:26 +0800

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,93 @@
 module.exports = function (grunt) {
+    var pkg = grunt.file.readJSON('package.json');
+    var cfg = {
+        src: './',
+        cwd: 'mobile',
+        serverHost: '0.0.0.0',
+        serverPort: 9000,
+        livereload: 35729
+    }; 
     grunt.initConfig({
+        pkg: pkg,
+        cfg: cfg,
+        connect: {
+          options: {
+            port: cfg.serverPort,
+            hostname: cfg.serverHost,
+            middleware: function(connect, options) {
+              return [
+                require('connect-livereload')({
+                  port: cfg.livereload
+                }),
+                // Serve static files.
+                connect.static(options.base),
+                // Make empty directories browsable.
+                // connect.directory(options.base),
+              ];
+            }
+          },
+          server: {
+            options: {
+              // keepalive: true,
+              base: cfg.src,
+            }
+          }
+        },
+        open: {
+          server: {
+            url: 'http://127.0.0.1:' + cfg.serverPort
+          }
+        },
+        watch: {
+              options: {
+                livereload: cfg.livereload,
+              },
+              scripts:  {
+                    files: [cfg.cwd +'/js/**'],
+                    tasks: ['jshint']
+                },
+            less:  {
+                    files: [cfg.cwd +'/css/**/*.less'],
+                    tasks: ['less']
+                },
+        },
+
+        less: {
+          combile: {
+            options: {
+              // paths: ["mobile/css"]
+            },
+            files: {
+              "mobile/css/main.css": "mobile/css/main.less"
+            }
+          }
+        },
+
         jshint: {
             options:  grunt.file.readJSON('.jshintrc'),
             hint: {
-                src: ['mobile/js/**/*.js', '!mobile/js/sea.js', '!mobile/js/handlebars.js']
+                src: ['mobile/js/**/*.js', '!mobile/js/sea.js', '!mobile/js/vendor/**']
             },
         },
 
         uglify : {
             ugly: {
-                cwd:'mobile/js',
-                src: ['**/*.js', '!/sea.js', '!/handlebars.js'],
+                src: ['mobile/js/**/*.js', '!mobile/js/sea.js', '!mobile/js/handlebars.js'],
                 dest: 'dest/uglify.js'
             }
         }
     });
 
     // Load the plugin
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-    // Default task(s)
+    //  task(s)
     grunt.registerTask('default', [
-        'jshint'
+        'connect:server', 
+        'open:server', 
+        'watch'
     ]);
-    grunt.registerTask('pack', [
+    grunt.registerTask('build', [
         'uglify'
     ]);
 };
--- mobile/js/init.js
+++ mobile/js/init.js
@@ -2,6 +2,9 @@ define(function (require) {
     var loading = require('unit/loading'),
         viewport = require('unit/viewport');
 
+
+
+
     viewport.show(localStorage.lastPanel || '#login');
     loading.hide();
 });
