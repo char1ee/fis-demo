9842f30f9b7e5270a72b8d948af9854743f26606
char1ee <a@div.li>
2013-08-19 18:43:34 +0800

    borwserify

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function (grunt) {
     var pkg = grunt.file.readJSON('package.json');
     var cfg = {
         src: './',
-        cwd: 'mobile',
+        cwd: './mobile',
         serverHost: '0.0.0.0',
         serverPort: 9000,
         livereload: 35729
@@ -76,22 +76,147 @@ module.exports = function (grunt) {
                 dest: 'dest/uglify.js'
             }
         },
-        combo: {
-            options: {
-                // sourceMap: {
-                //     // sourceRoot: '/src/'
-                // }
+        // combo: {
+        //     options: {
+        //         // sourceMap: {
+        //         //     // sourceRoot: '/src/'
+        //         // }
+        //     },
+        //     build: {
+        //         files: [{
+        //             expand: true,
+        //             cwd: 'test/src/',
+        //             src: '**/*.js',
+        //             dest: 'test/dist',
+        //             ext: '.combo.js'
+        //         }]
+        //     }
+        // },
+        // browserify: {
+        //     files: {
+        //         'mobile/js/bbbbbbbbbbbbb.js': [cfg.cwd + '/js/init.js']
+        //     },
+        //     options: {
+        //         transform: ['coffeeify']
+        //     }
+        // }
+        browserify2: {
+            // basic: {
+            //     src: ['test/fixtures/basic/*.js'],
+            //     dest: 'tmp/basic.js'
+            // },
+
+              // ignores: {
+              //   src: ['test/fixtures/ignore/*.js'],
+              //   dest: 'tmp/ignores.js',
+              //   options: {
+              //     ignore: ['test/fixtures/ignore/ignore.js', 'os']
+              //   }
+              // },
+
+              // alias: {
+              //   src: ['test/fixtures/alias/entry.js'],
+              //   dest: 'tmp/alias.js',
+              //   options: {
+              //     alias: ['test/fixtures/alias/toBeAliased.js:alias']
+              //   }
+              // },
+
+              // aliasString: {
+              //   src: ['test/fixtures/alias/entry.js'],
+              //   dest: 'tmp/aliasString.js',
+              //   options: {
+              //     alias: 'test/fixtures/alias/toBeAliased.js:alias'
+              //   }
+              // },
+
+              // aliasMappings: {
+              //   src: ['test/fixtures/aliasMappings/**/*.js'],
+              //   dest: 'tmp/aliasMappings.js',
+              //   options: {
+              //     aliasMappings: [
+              //       {
+              //         cwd: 'test/fixtures/aliasMappings/',
+              //         src: ['**/*.js'],
+              //         dest: 'tmp/shortcut/',
+              //         flatten: true
+              //       },
+              //       {
+              //         cwd: 'test/fixtures/aliasMappings/foo/',
+              //         src: ['**/*.js'],
+              //         dest: 'tmp/other/'
+              //       }
+              //     ]
+              //   }
+              // },
+
+              // external: {
+              //   src: ['test/fixtures/external/entry.js', 'text/fixtures/external/b.js'],
+              //   dest: 'tmp/external.js',
+              //   options: {
+              //     external: ['test/fixtures/external/a.js', 'events', 'vendor/alias']
+              //   }
+              // },
+
+              // 'external-dir': {
+              //   src: ['test/fixtures/external-dir/*.js'],
+              //   dest: 'tmp/external-dir.js',
+              //   options: {
+              //     external: ['test/fixtures/external-dir/b']
+              //   }
+              // },
+
+              // externalize: {
+              //   src: ['test/fixtures/externalize/b.js'],
+              //   dest: 'tmp/externalize.js',
+              //   options: {
+              //     alias: [
+              //       'test/fixtures/externalize/a.js:test/fixtures/externalize/a.js',
+              //       'events'
+              //     ]
+              //   }
+              // },
+
+              // noParse: {
+              //   src: ['test/fixtures/noParse/*.js'],
+              //   dest: 'tmp/noParse.js',
+              //   options: {
+              //     noParse: ['test/fixtures/noParse/jquery.js']
+              //   }
+              // },
+                compile: {
+                    entry: cfg.cwd + '/js/init.js',
+                    compile: 'mobile/js/aaaaaaaaaaaaaaaa.js',
+                },
+
+              // shim: {
+              //   src: ['test/fixtures/shim/a.js', 'test/fixtures/shim/shim.js'],
+              //   dest: 'tmp/shim.js',
+              //   options: {
+              //     shim: {
+              //       shimmedJQ: {
+              //         path: 'test/fixtures/shim/jquery.js',
+              //         exports: '$'
+              //       }
+              //     }
+              //   }
+              // },
+
+              // sourceMaps: {
+              //   src: ['test/fixtures/basic/*.js'],
+              //   dest: 'tmp/sourceMaps.js',
+              //   options: {
+              //     debug: true
+              //   }
+              // }
             },
-            build: {
-                files: [{
-                    expand: true,
-                    cwd: 'test/src/',
-                    src: '**/*.js',
-                    dest: 'test/dist',
-                    ext: '.combo.js'
-                }]
-            }
-        }
+            // dev: {
+            //     entry: './build/entry.js',
+            //     mount: '/application.js',
+            //     server: './build/server.js',
+            //     debug: true
+            // },
+
     });
 
     // Load the plugin
@@ -99,7 +224,6 @@ module.exports = function (grunt) {
 
     //  task(s)
     grunt.registerTask('default', [
-        'combo',
         'connect:server',
         'open:server',
         'watch'
@@ -109,6 +233,6 @@ module.exports = function (grunt) {
     ]);
     grunt.registerTask('build', [
         // 'uglify'
-        'combo'
+        'browserify2'
     ]);
 };
--- mobile/js/init.js
+++ mobile/js/init.js
@@ -1,7 +1,10 @@
 define(function (require) {
-    var loading = require('unit/loading'),
-        viewport = require('unit/viewport');
+	var $ = require('$');
+    var loading = require('./unit/loading'),
+        viewport = require('./unit/viewport');
 
     viewport.show(localStorage.lastPanel || '#login');
     loading.hide();
 });
+
+
